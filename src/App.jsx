c82dddac6b230b247d0bf4dcb7cbfe8c5bda675d
Santiago Leon <santiago.leon@binomio.uy>
2023-09-12 17:35:06 -0300

    Pagination using useScroll hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ImageList } from "./components/ImageList/ImageList";
 import style from "./App.module.css"
+import { useScrollPosition } from "./hooks/useScrollPosition";
+import axios from "axios";
 
 export function App() {
-  const [imageList, setImageList] = useState(DATA);
-  return <div className={style.root}>
-    <h1>Rand'images</h1>
-    <h2>Download random open source images</h2>
-    <ImageList imageList={imageList} />
-  </div>;
-}
+  const [imageList, setImageList] = useState([]);
+  const { isBottom } = useScrollPosition();
 
-  const DATA = [
-    {
-      id: "0",
-      author: "Alejandro Escamilla",
-      width: 5000,
-      height: 3333,
-      url: "https://unsplash.com/photos/yC-Yzbqy7PY",
-      download_url: "https://picsum.photos/id/0/5000/3333",
-    },
-    {
-      id: "1",
-      author: "Alejandro Escamilla",
-      width: 5000,
-      height: 3333,
-      url: "https://unsplash.com/photos/LNRyGwIJr5c",
-      download_url: "https://picsum.photos/id/1/5000/3333",
-    },
-    {
-      id: "2",
-      author: "Alejandro Escamilla",
-      width: 5000,
-      height: 3333,
-      url: "https://unsplash.com/photos/N7XodRrbzS0",
-      download_url: "https://picsum.photos/id/2/5000/3333",
-    },
-    {
-      id: "3",
-      author: "Alejandro Escamilla",
-      width: 5000,
-      height: 3333,
-      url: "https://unsplash.com/photos/Dl6jeyfihLk",
-      download_url: "https://picsum.photos/id/3/5000/3333",
-    },
-    {
-      id: "4",
-      author: "Alejandro Escamilla",
-      width: 5000,
-      height: 3333,
-      url: "https://unsplash.com/photos/y83Je1OC6Wc",
-      download_url: "https://picsum.photos/id/4/5000/3333",
-    },
-  ];
+  // 1- Store the page to fetch
+  const [pageToFetch, setPageToFetch] = useState(1);
+
+  // 3- Listen to pageToFetch updates amd request new images
+  useEffect(() => {
+    fetchImagesByPage(pageToFetch)
+  }, [pageToFetch])
+
+  // 5- Increate the page to fetch when reach bottom
+  useEffect(() => {
+    if (isBottom) {
+      console.log('increase page')
+      increasePage();
+    }
+  }, [isBottom]);
+
+  // 2- Create a function to request to fetch 5 others images
+  async function fetchImagesByPage(page) {
+    console.log("http request")
+    const { data } = await axios(
+      `https://picsum.photos/v2/list?page=${page}&limit=5`
+    );
+    setImageList([...imageList, ...data]);
+  }
+
+  // 4- Create a function to increase the page to fetch number
+  function increasePage() {
+    setPageToFetch(pageToFetch + 1);
+  }
+
+
+
+  return (
+    <div className={style.root}>
+      <h1>Rand'images</h1>
+      <h2>Download random open source images</h2>
+      <ImageList imageList={imageList} />
+    </div>
+  );
+}
--- src/components/ImageList/ImageList.jsx
+++ src/components/ImageList/ImageList.jsx
@@ -1,10 +1,11 @@
 import { ImageListItem } from "../ImageListItem/ImageListItem"
+import "./ImageList.css"
 
 export function ImageList({ imageList }) {
   return imageList.map(img => {
     return (
-      <div key={img.id}>
-        <ImageListItem img={img} />;
+      <div key={img.id} className="card_item">
+        <ImageListItem img={img} />
       </div>
     );
   });
